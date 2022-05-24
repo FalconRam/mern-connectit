ca99946bc51ea4e6ed265de279e55e51699a26f6
FalconRam <ramvignesh974@gamil.com>
2022-05-24 10:47:15 +0530

    Configuration of BE, FE

---
+++ server/server.js
@@ -0,0 +1,30 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express();
+
+app.use(
+  express.json({
+    limit: "30mb",
+    extended: true,
+  })
+);
+app.use(
+  express.urlencoded({
+    limit: "30mb",
+    extended: true,
+  })
+);
+app.use(cors());
+
+const port = process.env.PORT || 5000;
+const dbUri =
+  "mongodb+srv://dbRamvignesh:dbRammongodb99@cluster0.lznzg.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    app.listen(port, () => console.log(`Server is running on port: ${port}`));
+  })
+  .catch((err) => console.log(err));
