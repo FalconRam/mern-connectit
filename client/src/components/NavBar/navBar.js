29a0dd56229be2c24ee2561212b834643863bfd5
FalconRam <ramvignesh974@gamil.com>
2022-07-11 19:37:46 +0530

    Removed logout by token expiries

--- client/src/components/NavBar/navBar.js
+++ client/src/components/NavBar/navBar.js
@@ -2,35 +2,33 @@ import React, { useState, useEffect } from "react";
 import { Typography, AppBar, Toolbar, Button, Avatar } from "@material-ui/core";
 import { Link, useHistory, useLocation } from "react-router-dom";
 import { useDispatch } from "react-redux";
-import decode from "jwt-decode";
+// import decode from "jwt-decode";
 
 import { LOGOUT } from "../../constants/actionTypes";
 import logo from "../../images/logo.png";
 import useStyles from "./styles";
 
 const Navbar = () => {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
   const classes = useStyles();
   const dispatch = useDispatch();
   const history = useHistory();
-  const location = useLocation();
-
-  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
-  // console.log(user);
-
-  useEffect(() => {
-    const token = user?.token;
-    if (token) {
-      const decodedToken = decode(token);
-      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
-    }
-    setUser(JSON.parse(localStorage.getItem("profile")));
-  }, [location]);
+  // const location = useLocation();
 
   const logout = () => {
     dispatch({ type: LOGOUT });
-    setUser(null);
     history.push("/auth");
+    setUser(null);
   };
+
+  // useEffect(() => {
+  //   const token = user?.token;
+  //   if (token) {
+  //     const decodedToken = decode(token);
+  //     if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+  //   }
+  //   setUser(JSON.parse(localStorage.getItem("profile")));
+  // }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
@@ -52,7 +50,7 @@ const Navbar = () => {
         </Typography>
       </div>
       <Toolbar className={classes.toolBar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
               className={classes.purple}
