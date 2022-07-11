088f92e5ecdbf6573750b0ae300883b27e27e446
FalconRam <ramvignesh974@gamil.com>
2022-07-11 19:41:28 +0530

    Payload updated

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -53,8 +53,8 @@ export const getPostsBySearch = async (req, res) => {
 export const getPostsById = async (req, res) => {
   const { id } = req.params;
   try {
-    const postMessageById = await PostMessage.findById(id);
-    res.status(200).json({ data: postMessageById });
+    const post = await PostMessage.findById(id);
+    res.status(200).json(post);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
